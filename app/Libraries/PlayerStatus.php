3636a0485a894b3ef70813de58da0e78930e9f72
Dev Miduz <devmiduz@gmail.com>
2023-12-21 12:15:00 +0000

    Update PlayerStatus.php
    Added Doc Block comments.

--- app/Libraries/PlayerStatus.php
+++ app/Libraries/PlayerStatus.php
@@ -2,6 +2,10 @@
 
 namespace App\Libraries;
 
+/**
+ * An enum containing the possible values a Player's status can be.
+ * SEARCHING, PLAYING, TIMED_OUT, DISCONNECTED
+ */
 enum PlayerStatus: string {
     case SEARCHING = 'SEARCHING';
     case PLAYING = 'PLAYING';
